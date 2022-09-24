acc33d05c804fa1903b77f4b87186bd34470d58d
Tomáš Valášek <tomas.valasek@atreo.cz>
2022-09-24 17:17:57 +0200

    initial commit

---
+++ CoreDataMVVM/CoreDataMVVM/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  CoreDataMVVM
+//
+//  Created by Tomáš Valášek on 24.09.2022.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+            Text("Hello")
+        }
+    }
+}
---
+++ CoreDataMVVM/CoreDataMVVM/CoreDataMVVMApp.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataMVVMApp.swift
+//  CoreDataMVVM
+//
+//  Created by Tomáš Valášek on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMVVMApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
